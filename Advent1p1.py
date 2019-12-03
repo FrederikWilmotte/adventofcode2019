6757b8cd8e9dc52a7d25efb049abe2aa7c9c1872
Frederik Wilmotte <frederik.wilmotte@gmail.com>
2019-12-03 17:14:31 +0100

    Add files via upload

---
+++ Advent1p1.py
@@ -0,0 +1,8 @@
+#Advent1 - Part1
+#Day 1: The Tyranny of the Rocket Equation
+brandstof = 0
+bestand = open("Mass.txt","r")
+for lijn in bestand:
+    massa = int(lijn)
+    brandstof = brandstof + int(massa/3)-2
+print("Brandstof:",brandstof)
---
+++ Advent1p2.py
@@ -0,0 +1,11 @@
+#Advent1 - Part2
+#Day 1: The Tyranny of the Rocket Equation
+brandstof = 0
+bestand = open("Mass.txt","r")
+for lijn in bestand:
+    massa = int(lijn)
+    brandstofmassa = int(massa/3)-2
+    while brandstofmassa > 0:
+        brandstof = brandstof + brandstofmassa
+        brandstofmassa = int(brandstofmassa/3)-2
+print("Brandstof:",brandstof)
