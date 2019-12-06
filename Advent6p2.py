633fbbe4fb29c57ca0ced6708d951091eed2a4fd
Frederik Wilmotte <frederik.wilmotte@gmail.com>
2019-12-06 19:25:15 +0100

    Add files via upload

--- Advent6p2.py
+++ Advent6p2.py
@@ -1,45 +1,44 @@
 # Advent6 - Part2
 # Day 6: Universal Orbit Map
-orbits = []
+
+#Bepalen van de afstand tot object COM
+def orbit_afstand(objectSEARCH):
+    global orbit_stappen
+    orbit_stappen = []
+    while objectSEARCH != "COM":
+        gevonden = "nee"
+        i = 0
+        while i < len(objectA) and gevonden == "nee":
+            if objectB[i] == objectSEARCH:
+                gevonden = "ja"
+                orbit_stappen.append(objectA[i])
+                objectSEARCH = objectA[i]
+            i = i + 1
+    return orbit_stappen
+
+#declaraties
 objectA = []
 objectB = []
 wegNaarCOMYOU = []
 wegNaarCOMSAN = []
-objectYOU = "YOU"
-objectSAN = "SAN"
-objectCOM = "COM"
-objectSEARCH = ""
 orbitmap = open("Day6map.txt","r")
-afstand = 0
+
 #Laden van orbits is het geheugen
 for orbit in orbitmap:
-    orbits.append(orbit)
     objectA.append(orbit[:3])
     objectB.append(orbit[4:7])
+
 #Bepalen van de afstand van YOU tot COM
-objectSEARCH = objectYOU
-while objectSEARCH != objectCOM:
-    gevonden = "nee"
-    i = 0
-    while i < len(orbits) and gevonden == "nee":
-        if objectB[i] == objectSEARCH:
-          gevonden = "ja"
-          wegNaarCOMYOU.append(objectA[i])
-          objectSEARCH = objectA[i]
-        i = i + 1
+orbit_afstand("YOU")
+wegNaarCOMYOU = orbit_stappen
+
 #Bepalen van de afstand van YOU tot SAN
-objectSEARCH = objectSAN
-while objectSEARCH != objectCOM:
-    gevonden = "nee"
-    i = 0
-    while i < len(orbits) and gevonden == "nee":
-        if objectB[i] == objectSEARCH:
-          gevonden = "ja"
-          wegNaarCOMSAN.append(objectA[i])
-          objectSEARCH = objectA[i]
-        i = i + 1
-#Bepalen gemeenschappelijke orbiter
+orbit_afstand("SAN")
+wegNaarCOMSAN = orbit_stappen
+
+#Bepalen van de kortste afstand tot het gemeenschappelijke object
 gevonden = "nee"
+afstand = 0
 i = 0
 while i < len(wegNaarCOMYOU) and gevonden == "nee":
     j = 0
